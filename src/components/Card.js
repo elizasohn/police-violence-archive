71208da53d7852a477fed7aa55b4651ffa142a25
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-06-27 20:41:15 -0700

    add react native player react tweet embed spin up fake database

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,31 @@ import React from "react";
 import card from "../css/card.css";
 import cop from "../media/cop.jpg";
 import cop2 from "../media/cop2.jpg";
+import ReactPlayer from "react-player";
+import { FAKE_DATABASE } from "../constants/FakeDatabase.js";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
+import { withInheritedProps } from "object-path";
 
-const Card = () => {
+
+
+
+const Card = ( ) => {
+
+  const POSTS = FAKE_DATABASE.posts;
+  console.log("POSTS", POSTS[0].tweetURL);
+
+
   return (
     <div className="cardWrapper">
       <div className="cardHeader">
@@ -12,23 +35,27 @@ const Card = () => {
         </div>
 
         <div className="cardHeaderColTwo">
-          <h5>Date</h5>
+          <h5>Date: {POSTS[0].twitterDate}</h5>
+          <h5>Time: {POSTS[0].twitterTime}</h5>
+          <h5>
+            Location: {POSTS[0].city} , {POSTS[0].state}
+          </h5>
         </div>
 
         <div className="cardHeaderColThree">
-          <h5>Bludgeoned</h5>
-          <h5>Pepper Sprayed</h5>
-          <h5>Tear Gassed</h5>
+          <h5>{POSTS[0].text}</h5>
         </div>
       </div>
 
       <div className="cardBody">
         <div className="cardBodyColOne">
-          <h5>Tweet Information</h5>
+          <TwitterTweetEmbed
+            tweetId={POSTS[0].tweetURL.split("/")[5]}
+          ></TwitterTweetEmbed>
         </div>
 
         <div className="cardBodyColTwo">
-          <img className="embeddedVideoPlayer" src={cop2} />
+          <ReactPlayer className="reactPlayer" url={POSTS[0].youTube} />
         </div>
       </div>
 
---
+++ src/constants/FakeDatabase.js
@@ -0,0 +1,46 @@
+export const FAKE_DATABASE = {
+         posts: [
+           {
+             tgdNumber: 0.01,
+             city: "Portland",
+             state: "Oregon",
+             tweetURL:
+               "https://twitter.com/greg_doucette/status/1272261995874725888",
+             fileName: "Greg Doucette #NA - Portland OR.mp4",
+             youTube:
+               "https://www.youtube.com/watch?v=F4_yJjdsJ_0&feature=youtu.be",
+             text:
+               "as of now this is assumed to be a non-cop so no number, but a white vigilante spends his time circling the block trying to run over protestors (hitting several non-protestor cars along the way) [@kevin_parks]",
+             twitterDate: "200617",
+             twitterTime: "8:31:00 AM	",
+           },
+           {
+             tgdNumber: 0.02,
+             city: "Houston",
+             state: "Texas",
+             tweetURL:
+               "https://twitter.com/greg_doucette/status/1266565461937917953",
+             fileName: "Greg Doucette #NA - Houston TX.mp4",
+             youTube:
+               "https://www.youtube.com/watch?v=suTGneu7tZU&feature=youtu.be",
+             text:
+               "Shoving an unarmed woman to the curb, prompting a seizure that put her in the emergency room",
+             twitterDate: "200530",
+             twitterTime: "8:25:00 AM",
+           },
+           {
+             tgdNumber: 0.03,
+             city: "Chicago",
+             state: "Illinois",
+             tweetURL:
+               "https://https://twitter.com/greg_doucette/status/1266753089517821954.com/greg_doucette/status/1266565461937917953",
+             fileName: "Greg Doucette #NA - Chicgao IL.mp4",
+             youTube:
+               "https://www.https://www.youtube.com/watch?v=Eu1KRskL-0E&feature=youtu.be.com/watch?v=suTGneu7tZU&feature=youtu.be",
+             text:
+               "Shoving pepper-spraying a variety of unarmed protestors for sport	 unarmed woman to the curb, prompting a seizure that put her in the emergency room",
+             twitterDate: "200530",
+             twitterTime: "6:25:00 PM",
+           },
+         ],
+       };
